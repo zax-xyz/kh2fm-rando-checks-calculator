accec8a9bf0799cb68134d07aae5ae8979442f04
Michael Vo <zax@zaxu.xyz>
2021-03-15 00:13:02 +1100

    Show numbers next to settings and extract in JS from DOM

--- script.js
+++ script.js
@@ -1,78 +1,34 @@
-const values = {
-  settings: {
-    "Critical Mode": 7,
-    Leveling: 23,
-  },
-
-  worlds: {
-    "Simulated Twilight Town": 25,
-    "Twilight Town": 50,
-    "Hollow Bastion": 39,
-    "Cavern of Remembrance": 21,
-    "Beast's Castle": 28,
-    "Olympus Coliseum": 31,
-    Agrabah: 32,
-    "Land of Dragons": 28,
-    "100 Acre Wood": 24,
-    Atlantica: 4,
-    "Pride Lands": 28,
-    "Disney Castle": 10,
-    "Timeless River": 12,
-    "Halloween Town": 19,
-    "Port Royal": 30,
-    "Space Paranoids": 18,
-    "The World That Never Was": 30,
-  },
-
-  include: {
-    "Absent Silhouettes": 5,
-    "Data Organization XIII": 13,
-    "Olympus Cups": 8,
-    "Hades Cup": 1,
-    "Lingering Will (Terra)": 2,
-    Sephiroth: 1,
-    "Form Abilities": 15,
-    "Form Growth Abilities": 15,
-    "GoA Chests": 3,
-  },
-};
-
-let checks = 0;
+let total = 0;
 
 ["settings", "worlds", "include"].forEach(category => {
   document.querySelectorAll(`#${category} li`).forEach(e => {
-    const name = e.querySelector("h1").innerText;
+    const checks = Number(e.querySelector("h1 span").innerText);
     const input = e.querySelector("input");
 
     if (input.checked) {
-      checks += values[category][name];
+      total += Number(checks);
     }
   });
 });
 
 const checksElem = document.getElementById("checks");
-checksElem.innerText = checks;
+checksElem.innerText = total;
 
 document.querySelectorAll("section input").forEach(e => {
   e.addEventListener("change", e => {
     const category = e.target.closest("section").id;
-    const name = e.target.closest("li").querySelector("h1").innerText;
-    const value = values[category][name];
+    const checks = Number(e.target.closest("li").querySelector("h1 span").innerText);
 
     if (e.target.checked) {
-      checks += value;
+      total += checks;
     } else {
-      checks -= value;
+      total -= checks;
     }
 
-    checksElem.innerText = checks;
+    checksElem.innerText = total;
   });
 });
 
 document.getElementById("toggle-all-btn").addEventListener("click", () => {
-  document.querySelectorAll("section input").forEach(e => {
-    if (e.checked) {
-      e.click();
-    }
-  });
+  document.querySelectorAll("section input:checked").forEach(e => e.click());
 });
